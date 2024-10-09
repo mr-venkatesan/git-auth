f785665e86b58070f7c4fae49e687f10c9bd99cc
SmileyVK <sowndharyathillai@gmail.com>
2024-10-09 08:06:48 +0530

    add drawer in home page

--- lib/view/pages/home_view.dart
+++ lib/view/pages/home_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:git_auth/bloc/git_bloc/git_bloc.dart';
@@ -6,7 +5,6 @@ import 'package:git_auth/utils/app_color.dart';
 
 import '../../router/routes.dart';
 import '../widgets/project_card_view.dart';
-
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -16,7 +14,8 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  GitBloc gitBloc=GitBloc();
+  GitBloc gitBloc = GitBloc();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   void initState() {
     super.initState();
@@ -25,17 +24,19 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
     return Scaffold(
-      backgroundColor: AppColor.bgPrimaryColor,
+      key: scaffoldKey,
+      backgroundColor: AppColor.primaryColor,
+      drawerEnableOpenDragGesture: false,
       appBar: AppBar(
         elevation: 0,
-        shadowColor: Colors.transparent,
         backgroundColor: AppColor.primaryColor,
         surfaceTintColor: AppColor.primaryColor,
         leading: IconButton(
           onPressed: () {
-            // Add functionality to open a drawer or perform another action
+            scaffoldKey.currentState?.openDrawer();
           },
           icon: const Icon(
             Icons.menu,
@@ -62,6 +63,7 @@ class _HomeViewState extends State<HomeView> {
         ],
       ),
       body: buildHomePage(),
+      drawer: CustomDrawer(),
     );
   }
 
@@ -87,131 +89,284 @@ class _HomeViewState extends State<HomeView> {
       ),
     );
   }
-  Widget buildHomePage(){
-    double topPad = MediaQuery.of(context).padding.top;
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColor.bgPrimaryColor
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 200,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                // Background color transition between primary and secondary colors
-                Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 100,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: AppColor.primaryColor,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 20.0),
-                          child: Text(
-                            "Hi Venkatesan",
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                color: AppColor.textSecondaryColor
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 100,
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: AppColor.bgPrimaryColor,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 140,
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 20.0),
-                  margin: const EdgeInsets.all(15),
+
+  Widget buildHomePage() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: DecoratedBox(
                   decoration: BoxDecoration(
-                    color: Colors.white, // Background color of the card
-                    borderRadius: BorderRadius.circular(16.0), // Rounded corners
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2), // Shadow color
-                        spreadRadius: 2, // How much the shadow spreads
-                        blurRadius: 8, // Softness of the shadow
-                        offset: const Offset(0, 4), // Offset of the shadow (x, y)
-                      ),
-                    ],
+                    color: AppColor.primaryColor,
                   ),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 48,
-                        height: 48,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFFE4F7F7),
-                        ),
-                        child: const Icon(Icons.business, color: Color(0xFF00C2AE)),
-                      ),
-                      const SizedBox(width: 16),
-                      Column(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "Hi Venkatesan",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.textSecondaryColor),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                  top: 90,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: AppColor.bgPrimaryColor,
+                      )
+                  )
+              ),
+              Positioned(
+                  top: 180,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColor.bgPrimaryColor,
+                    ),
+                    child: Container(
+                      color: AppColor.bgPrimaryColor,
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text(
-                            'Organisation Name',
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Projects',
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
                           ),
                           const SizedBox(height: 8),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color(0xFF00C2AE),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            padding: const EdgeInsets.all(12.0),
-                            child: const Text(
-                              'VGTS',
-                              style: TextStyle(color: Colors.white, fontSize: 12,fontWeight: FontWeight.w500),
+                          Expanded(
+                            child: ListView(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              children: [
+                                ProjectCardView(
+                                  title: 'Heavenly',
+                                  subtitle: 'Rajesh kannan',
+                                  onTap: () => Navigator.pushNamed(context, Routes.repo),
+                                ),
+                                ProjectCardView(
+                                  title: 'Surgtest',
+                                  subtitle: 'Vijay',
+                                  onTap: () => Navigator.pushNamed(context, Routes.repo),
+                                ),
+                                ProjectCardView(
+                                  title: 'TNULM',
+                                  subtitle: 'Vikky',
+                                  onTap: () => Navigator.pushNamed(context, Routes.repo),
+                                ),
+                                ProjectCardView(
+                                  title: 'Erp one',
+                                  subtitle: 'Vikky',
+                                  onTap: () => Navigator.pushNamed(context, Routes.repo),
+                                ),
+                                ProjectCardView(
+                                  title: 'Aggromalie',
+                                  subtitle: 'Rajesh kannan',
+                                  onTap: () => Navigator.pushNamed(context, Routes.repo),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                )
-              ],
+              ),
+              Container(
+                height: 140,
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+                margin: const EdgeInsets.symmetric(horizontal: 15,vertical: 25),
+                decoration: BoxDecoration(
+                  color: Colors.white, // Background color of the card
+                  borderRadius: BorderRadius.circular(16.0), // Rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2), // Shadow color
+                      spreadRadius: 2, // How much the shadow spreads
+                      blurRadius: 8, // Softness of the shadow
+                      offset: const Offset(0, 4), // Offset of the shadow (x, y)
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 48,
+                      height: 48,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFE4F7F7),
+                      ),
+                      child:
+                          const Icon(Icons.business, color: Color(0xFF00C2AE)),
+                    ),
+                    const SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Organisation Name',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF00C2AE),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: const EdgeInsets.all(12.0),
+                          child: const Text(
+                            'VGTS',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+}
+
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(color: Colors.white),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage(
+                        'assets/profile.jpg'), // Replace with your image asset
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    'REPO NAME',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 8,
+                    ),
+                    child: const Text(
+                      'SanthoshVGTS',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              'Projects',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 8),
           Expanded(
             child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: EdgeInsets.zero,
               children: [
-                ProjectCardView(title: 'Heavenly', subtitle: 'Rajesh kannan', onTap: ()=>Navigator.pushNamed(context,Routes.repo),),
-                ProjectCardView(title: 'Surgtest', subtitle: 'Vijay', onTap: ()=>Navigator.pushNamed(context,Routes.repo),),
-                ProjectCardView(title: 'TNULM', subtitle: 'Vikky', onTap: ()=>Navigator.pushNamed(context,Routes.repo),),
-                ProjectCardView(title: 'Erp one', subtitle: 'Vikky', onTap: ()=>Navigator.pushNamed(context,Routes.repo),),
-                ProjectCardView(title: 'Aggromalie', subtitle: 'Rajesh kannan', onTap: ()=>Navigator.pushNamed(context,Routes.repo),),
+                _buildDrawerItem(
+                  icon: Icons.business,
+                  text: 'Vithea',
+                  onTap: () {
+                    // Add your navigation logic here
+                  },
+                ),
+                _buildDrawerItem(
+                  icon: Icons.description,
+                  text: 'Yolo works',
+                  onTap: () {
+                    // Add your navigation logic here
+                  },
+                ),
+                _buildDrawerItem(
+                  icon: Icons.book,
+                  text: 'One gold',
+                  onTap: () {
+                    // Add your navigation logic here
+                  },
+                ),
+                _buildDrawerItem(
+                  icon: Icons.book_online,
+                  text: 'Zoho books',
+                  onTap: () {
+                    // Add your navigation logic here
+                  },
+                ),
+                const Divider(),
+                _buildDrawerItem(
+                  icon: Icons.logout,
+                  text: 'Logout',
+                  onTap: () {
+                    // Add your logout logic here
+                  },
+                ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildDrawerItem({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: const Color(0xFFE4F7F7),
+        child: Icon(icon, color: const Color(0xFF00C2AE)),
+      ),
+      title: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      onTap: onTap,
     );
   }
 }
